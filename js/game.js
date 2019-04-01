7d66e10ef0f3200aa01acd3e2a93e1b583a02fbf
Nyesha Jones <neeshajolmfao@gmail.com>
2019-04-01 22:22:10 +0000

    Finsihed for the most part!

--- js/game.js
+++ js/game.js
@@ -1,43 +1,81 @@
 // NOTE - You must have a level with the name "start".  This is used as the first level in the game.
 
 var game = {
-    music: "98_Lost_Mine.mp3",
-    background_image: "intro-bg.jpg",
+    music: "",
+    background_image: "",
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "You wake up in the anime Boku no Hero Academia! After you unfourtantly hit your head! What will you do first?",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "Go to School!",
+                    nextLevel: "school",
                 },
 
                 {
-                    text: "Keep on moving",
-                    nextLevel: "field",
+                    text: "Explore the town!",
+                    nextLevel: "town",
                 },
             ]
         },
 
-        cave: {
+        town: {
             background_image: "fire.gif",
-            music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+            music: "",
+            message: "You come across a friendly blonde haired blue eyed boy named Mirio Togata! Mirio tells you that he'll be heading to school very soon and asks if you want to come with him? Will you!",
+          
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "Go with him!",
+                    nextLevel: "school",
+                },
+                
+                 {
+                    text: "Leave",
+                    nextLevel: "intermediate",
+                },
+            ]
+        },
+        
+        school: {
+            background_image: "fire.gif",
+            music: "",
+            message: "Once at UA High School you meet Izuku Midoriya, Shoto Todoroki and Ochaco Uraraka. They were all very friendly and welcoming for the most part. They inform you that they have to head inside soon to go to class! Will you stay with them?",
+          
+            choices: [
+                {
+                    text: "Go to class with them!",
+                    nextLevel: "classroom",
+                },
+                
+                 {
+                    text: "Leave",
+                    nextLevel: "intermediate",
                 },
             ]
         },
 
-        field: {
-            message: "Some adventurer you are...",
+        classroom: {
+            message: "Heading inside with them you meet even more people! You meet Superheros All Might and Eraserhead! and even more students! What an exciting day this turned out to be! Now what shall you do?",
             choices: [
                 {
-                    text: "Start over",
+                    text: "Go home!",
+                    nextLevel: "intermediate",
+                },
+            ]
+        },
+        
+         intermediate: {
+            message: "Oh no! You've hit your head while walking past a tree! You wake up on the ground in central park! You did not fulfill your superhero duties! Wanna try again? ",
+            choices: [
+                {
+                    text: "Yes!",
                     nextLevel: "start",
+                },
+                   { 
+                    text: "Nope",
+                    nextLevel:"",
                 },
             ]
         },
